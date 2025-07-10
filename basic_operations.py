f3edef759fe03efe32b3ec13780bf80ce4576b90
Tranostc <tranostc@gmail.com>
2025-07-10 16:24:41 +0200

    Python Intro

---
+++ basic_operations.py
@@ -0,0 +1,16 @@
+# define the two variables
+number1 = 10
+number2 = 5
+
+# calculate sum
+sum_result = number1 + number2
+
+# calculate difference 
+difference_result = number1 - number2
+
+#calculate product
+product_result = number1 * number2
+
+print (f"sum of {number1} and {number2} is {sum_result}.")
+print (f"difference of {number1} and {number2} is {difference_result}.")
+print (f"product of {number1} and {number2} is {product_result}.")
