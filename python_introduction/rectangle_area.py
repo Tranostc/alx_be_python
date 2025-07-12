a7a50bd5738b2d4bb0c935535aaa89a1ed8a4e6f
Tranostc <tranostc@gmail.com>
2025-07-13 00:55:22 +0200

    python_introduction

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,6 @@
+length = 10
+width = 5
+
+# Calculate the area of the rectangle
+area = length * width
+print(f"The area of the rectangle is: {area}")
---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000  # Principal amount in dollars
+rate  = 0.05  # Interest rate (5%)
+time = 3  # Time in years
+
+# Calculate simple interest
+interest = principal * rate * time
+print(f"The simple interest is: {interest}")
+
